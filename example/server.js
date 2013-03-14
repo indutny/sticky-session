dcae5b8f724f19523cd136fe23879f6d262eca6a
Fedor Indutny <fedor.indutny@gmail.com>
2013-03-14 11:54:53 +0400

    lib: update to support 0.10

--- example/server.js
+++ example/server.js
@@ -2,7 +2,7 @@ var http = require('http'),
     sticky = require('../');
 
 sticky(http.createServer(function(req, res) {
-  res.end('worker: ' + process.env.NODE_WORKER_ID);
+  res.end('worker: ' + process.pid);
 })).listen(3000, function() {
   console.log('server started on 3000 port');
 });
